5aafacdf3ebc05cf6cc1edb45ed3a8fb4f3bafba
M <michael.halligan@hotschedules.com>
2017-01-19 15:26:17 +0200

    INFRA-103: Set a default certificate path to the openldap certificate, change variable name from cert to certificate, don't create a new cert file

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,16 +13,17 @@ class java_ks(
   $truststore_password  = getvar("::java_ks::params::ssl::truststore_password"),
   $user                 = getvar("::java_ks::params::ssl::user"),
   $group                = getvar("::java_ks::params::ssl::group"),
+  $certificate          = getvar("::java_ks::params::ssl::cert"),
   $truststoreconf       = hiera_hash("java_ks::config")
 
 ) inherits java_ks::params {
 
   validate_absolute_path($truststore)
   validate_string($truststore_password)
-  validate_hash($truststoreconf)
   validate_string($user)
   validate_string($group)
-
+  validate_absolute_path($certificate)
+  validate_hash($truststoreconf)
 
   contain 'java_ks::config'
 }
--- manifests/params/ssl.pp
+++ manifests/params/ssl.pp
@@ -3,9 +3,10 @@
 # This private class is called from java_ks::config used to manage the ssl properties if ssl is enabled
 #
 class java_ks::params::ssl {
+  $cert                = '/etc/openldap/cacerts/cacert.pem'
+  $config              = {} 
+  $group               = '' 
   $truststore          = '/tmp/truststore'
   $truststore_password = ''
-  $config      = {} 
   $user                = '' 
-  $group               = '' 
 }
