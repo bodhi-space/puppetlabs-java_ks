fe0e65e0f6c314eb34d385a2db9a5f63fc3f4079
Alex Yamauchi <alex.yamauchi@hotschedules.com>
2017-08-18 20:27:30 +0000

    UNRECOGNIZED TICKET TYPE!INFRA-5197: hacking in a way to get the needed class dependencies and notifications implemented

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,8 @@ class java_ks(
   $user                 = getvar("::java_ks::params::ssl::user"),
   $group                = getvar("::java_ks::params::ssl::group"),
   $certificate          = getvar("::java_ks::params::ssl::certificate"),
-  $truststore_keys      = hiera_hash("java_ks::truststore_keys")
+  $truststore_keys      = hiera_hash("java_ks::truststore_keys",{}),
+  $config_class_options = hiera_hash("java_ks::config_class_options",{})
 
 ) inherits java_ks::params {
 
@@ -25,5 +26,15 @@ class java_ks(
   validate_absolute_path($certificate)
   validate_hash($truststore_keys)
 
-  contain 'java_ks::config'
+  contain 'java_ks::config' 
+
+  if has_key($config_class_options, 'before') {
+    $config_class_options['before'] -> Class['java_ks::config'] 
+  }
+  if has_key($config_class_options, 'notify') {
+    Class['java_ks::config'] ~> $config_class_options['notify']
+  }
+  if has_key($config_class_options, 'before') {
+    Class['java_ks::config'] -> $config_class_options['before']
+  }
 }
